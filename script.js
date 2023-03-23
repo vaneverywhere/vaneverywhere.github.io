5c71a3aa5a99401c59de2af828ae42e6a023e82e
vaneverywhere <100239781+vaneverywhere@users.noreply.github.com>
2023-03-24 00:04:04 +0100

    Updated menu
    Added menu items highlighting

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ window.addEventListener("scroll", function() {
   var cards = document.querySelectorAll(".card");
   var top = window.pageYOffset;
   var bottom = top + window.innerHeight;
+  var threshold = 450; // adjust this value to change the threshold
   cards.forEach(function(card) {
     var position = card.getBoundingClientRect();
-    if (position.top < bottom && position.bottom > top) {
+    if (position.top < bottom - threshold && position.bottom > top) {
       card.classList.add("fade-in");
     }
   });
